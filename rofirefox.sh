cc14b76c4cea3263bd8c421f479089503271847b
Ethem <ethmtrgt@gmail.com>
2022-04-25 14:44:45 +0300

    sort profiles Add option to sort profiles alphabetically

--- rofirefox.sh
+++ rofirefox.sh
@@ -2,7 +2,7 @@
 
 CONFIG_PATH=~/.mozilla/firefox/profiles.ini
 
-choice=$(awk -F "=" '/Name/ { print $2 }' $CONFIG_PATH | rofi -dmenu -p "Select a profile...")
+choice=$(awk -F "=" '/Name/ { print $2 }' $CONFIG_PATH | sort | rofi -dmenu -p "Select a profile...")
 if [ "$choice" != "" ]; then
     firefox -P $choice &
 else
